bd8713e1c5c811d88ada51a6c96af3c6545fd211
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2024-09-15 08:40:01 -0300

    rearrangingFolders

---
+++ app/dist/js/views/view.js
@@ -0,0 +1,22 @@
+export class View {
+    constructor(seletor, escapar) {
+        this.escapar = false;
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento;
+        }
+        else {
+            throw new Error(`Seletor ${seletor} não existe!`);
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+    update(model) {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.elemento.innerHTML = template;
+    }
+}
